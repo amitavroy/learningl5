3a54137fcc18e30a51d7c4f82685c0ee047e6b67
Amitav Roy <reachme@amitavroy.com>
2015-05-29 22:39:28 +0530

    Added the code to validate the FB session and get the user object based on the access token.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,7 +1,9 @@
 <?php namespace App\Http\Controllers;
 
 use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
 use Facebook\FacebookSession;
+use Facebook\GraphUser;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller {
@@ -45,7 +47,39 @@ class WelcomeController extends Controller {
 
     public function getFBPostData(Request $request)
     {
-        dump($request->input());
+        session_start();
+//        dump($request->input());
+        $code = $request->input('code');
+        $user_data = [];
+
+        FacebookSession::setDefaultApplication(env('FB_APP_ID'), env('FB_APP_SECRET'));
+        $helper = new FacebookRedirectLoginHelper(env('FB_URL'));
+        $session = $helper->getSessionFromRedirect();
+//        dump($session);
+
+        if ($session) {
+            try {
+                $user_profile = (new FacebookRequest(
+                    $session, 'GET', '/me'
+                ))->execute()->getGraphObject(GraphUser::className());
+//                dump($user_profile);
+                $user_data['id'] = $user_profile->getProperty('id');
+                $user_data['email'] = $user_profile->getProperty('email');
+                $user_data['first_name'] = $user_profile->getProperty('first_name');
+                $user_data['gender'] = $user_profile->getProperty('gender');
+                $user_data['last_name'] = $user_profile->getProperty('last_name');
+                $user_data['name'] = $user_profile->getProperty('name');
+
+            } catch (FacebookRequestException $e) {
+                echo "Exception occured, code: " . $e->getCode();
+                echo " with message: " . $e->getMessage();
+            }
+        }
+
+        return response([
+            'code' => $code,
+            'user_data' => $user_data
+        ], 201);
     }
 
 }
