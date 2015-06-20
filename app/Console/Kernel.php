f6cf72d3413eacefa3f7371ae52890008675c331
Amitav Roy <reachme@amitavroy.com>
2015-06-20 22:49:01 +0530

    Adding the code for the tutorial

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected $commands = [
 		'App\Console\Commands\Inspire',
+		'App\Console\Commands\LogDemo',
 	];
 
 	/**
@@ -24,6 +25,8 @@ class Kernel extends ConsoleKernel {
 	{
 		$schedule->command('inspire')
 				 ->hourly();
+
+		$schedule->command('log:demo')->everyMinute();
 	}
 
 }
