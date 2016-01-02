749009599695599bf3c72eab383a7956b39601e4
Amitav Roy <reachme@amitavroy.com>
2016-01-02 21:08:57 +0530

    Adding the Model and Migration file

---
+++ database/migrations/2016_01_02_153446_create_invitations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateInvitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('invite_code');
+            $table->integer('inviter_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invitations');
+    }
+}
